cb6559e89318d41dad61e54d4a78b9602e6142d1
jcuacuas <jcuacuas.ptech@gmail.com>
2017-08-03 14:34:52 -0400

    added i^2cmaster

--- tool.py
+++ tool.py
@@ -2,10 +2,15 @@
 import subprocess
 
 #PIPING COMMANDS
-p1 = subprocess.Popen(['ls', '-s'], stdout=subprocess.PIPE)
-p2 = subprocess.Popen(['grep', '.py'], stdin=p1.stdout, stdout=subprocess.PIPE)
+cmd = 'makespsystemcall -raw "iicmaster -b /dev/iic/L02C0E06P14 -s -a 0x80 -o 0x00 -r 1"'
+cmd1 = 'grep -v [:digit:]'
+
+p1 = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
+p2 = subprocess.Popen(cmd1, stdin=p1.stdout, shell=True, stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
 p1.stdout.close()
 
 out,err = p2.communicate()
 
-print "\n",out
+print out
