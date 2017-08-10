f421ecaf34c81391a1473b0abe0cae8dd177eb8f
jcuacuas <jcuacuas.ptech@gmail.com>
2017-08-10 15:37:22 -0400

    Bit by bit register reading

--- testing.py
+++ testing.py
@@ -9,3 +9,54 @@ p1.stdout.close()
 out,err = p2.communicate()
 
 print "\n",out
+
+Capabilities1 = '0x0F'
+
+
+def int2bin(i):
+    if i == '0x0F': return 0x0F
+
+
+#print int2bin(Capabilities1)
+
+def on_off(i):
+    if i == '1':return True
+    else: return False
+    print i
+
+First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eight = '0'*8
+D = {'Values': [First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eight] }
+
+
+scale = 16
+num_of_bits = 8
+test = bin(int('0x0F', scale))[2:].zfill(num_of_bits)
+test = test[::-1]
+print test
+
+#print test[5]
+#print on_off(test[1])
+
+step = 0
+for num_of_bits in test:
+    if on_off(test[step]) == True:
+        D['Values'][step] = True
+        print D['Values'][step]
+    else:
+        D['Values'][step] = False
+        print D['Values'][step]
+    step = step + 1
+
+print '-'*len('hello')
+
+mask = 0b01111111
+byte_from_file = 0b10101010
+value = mask & byte_from_file
+#print bin(value)
+
+byteval = 0x0F
+idx = 1
+
+def get_bit(byteval,idx):
+    return ((byteval&(1<<idx))!=0);
+#print get_bit(byteval,idx)
--- tool.py
+++ tool.py
@@ -25,6 +25,9 @@ Slot1_Fwrev0 = '00'
 Slot1_Fwrev1 = '00'
 Slot0_Subfwrev = '00'
 Slot1_Subfwrev = '00'
+Capabilities0 = '0F'
+Capabilities1 = '01'
+Energy_Source_Policy = '03'
 
 if Vendor_Start_Pages == Max_Pages:
     Vendor_Start_Des = 'There are no vendor pages'
@@ -85,5 +88,110 @@ D = {'Registers': [Current_Page, Max_Pages,
 
 step = 0
 for i in D['Registers']:
+    if step == 0:
+        x = 'Paging Mechanism Registers'
+        print '-'*len(x) + '\nPaging Mechanism Registers \n'+ '-'*len(x)
+    if step == 4:
+        x = 'Version Registers'
+        print '-'*len(x) + '\nVersion Registers \n'+ '-'*len(x)
     print D['Descriptions'][step],D['Registers'][step]
     step = step + 1
+
+x = 'Characteristics Registers'
+print '-'*len(x) + '\nCharacteristics Registers \n'+ '-'*len(x)
+
+def on_off(i):
+    if i == '1':return True
+    else: return False
+    print i
+
+First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eight = '0'*8
+D = {'Values': [First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eight] }
+
+
+scale = 16
+num_of_bits = 8
+num_of_bits1 = 4
+Capabilities0_Test = bin(int(Capabilities0, scale))[2:].zfill(num_of_bits)
+Capabilities0_Test = Capabilities0_Test[::-1]
+
+step = 0
+for num_of_bits in Capabilities0_Test:
+    if on_off(Capabilities0_Test[step]) == True:
+        D['Values'][step] = 1
+    else:
+        D['Values'][step] = 0
+    step = step + 1
+
+print '\n' + '-'*5 + ' CAPABILITIES0 - Offset0x10 ' + '-'*5 + '\n'
+if D['Values'][0]:
+    print 'Module supports asserting the EVENT_n pin when an enabled event occurs'
+if D['Values'][1]:
+    print 'Module performs periodic Energy Source health checks'
+if D['Values'][2]:
+    print 'Module supports error injection'
+if D['Values'][3]:
+    print 'Module supports collecting the device statistics'
+if D['Values'][4]:
+    print 'Module supports I2C Block Read transactions and I2C Block Write transactions for Typed Block Data'
+if D['Values'][5]:
+    print 'Module supports two-pulse SAVE_n trigger mode'
+if D['Values'][6]:
+    print 'Module supports three-pulse SAVE_n trigger mode'
+if D['Values'][7]:
+    print 'Module supports the LCOM interface'
+
+
+
+scale = 16
+num_of_bits = 8
+num_of_bits1 = 4
+Capabilities1_Test = bin(int(Capabilities1, scale))[2:].zfill(num_of_bits)
+Capabilities1_Test = Capabilities1_Test[::-1]
+
+step = 0
+for num_of_bits in Capabilities1_Test:
+    if on_off(Capabilities1_Test[step]) == True:
+        D['Values'][step] = 1
+    else:
+        D['Values'][step] = 0
+    step = step + 1
+
+print '\n' + '-'*5 + ' CAPABILITIES1 - Offset0x11 ' + '-'*5 + '\n'
+if D['Values'][0]:
+    print 'Module supports the Atomic Arm and Erase operation'
+if D['Values'][1]:
+    print 'ABORT_CMD_TIMEOUT register is in units of seconds'
+if not D['Values'][1]:
+    print 'ABORT_CMD_TIMEOUT register is in units of milliseconds'
+if D['Values'][2]:
+    print 'Module supports an Operational Unit buffer for each Typed Block Data type'
+if D['Values'][3]:
+    print 'Module supports I2C Word Read transactions and I2C Word Write transactions for Typed Block Data'
+if D['Values'][4] == D['Values'][5] == 0:
+    print 'Reset Controller operation semantics are not reported'
+elif D['Values'][4] == D['Values'][5] == 1:
+    print 'Reserved'
+elif D['Values'][4]:
+    print 'Reset Controller operation honors SAVE_n'
+elif D['Values'][5]:
+    print 'Reset Controller operation does not honor SAVE_n'
+
+Energy_Source_Policy_Test = bin(int(Energy_Source_Policy, scale))[2:].zfill(num_of_bits1)
+Energy_Source_Policy_Test = Energy_Source_Policy_Test[::-1]
+step = 0
+for num_of_bits1 in Energy_Source_Policy_Test:
+    if on_off(Energy_Source_Policy_Test[step]) == True:
+        D['Values'][step] = 1
+    else:
+        D['Values'][step] = 0
+    step = step + 1
+
+print '\n' + '-'*5 + ' ENERGY_SOURCE_POLICY_TEST - Offset0x14 ' + '-'*5 + '\n'
+
+if D['Values'][0]:
+    print 'Device Managed Policy supported'
+if D['Values'][1]:
+    print 'Host Managed Policy supported'
+
+print '\n' + '-'*5 + ' HOST_MAX_OPERATION_RETRY - Offset0x15 ' + '-'*5 + '\n'
